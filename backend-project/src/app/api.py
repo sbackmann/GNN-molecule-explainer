8b227f9d746e6e738973fc74d77cbece804cf80a
Matthias Gabathuler <matthias.gabathuler@inf.ethz.ch>
2022-03-30 14:09:12 +0200

    make backend context aware

--- backend-project/src/app/api.py
+++ backend-project/src/app/api.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 import pandas as pd
 import os
-import csv
-import codecs
 from io import StringIO
 # from pydantic_models.example_data_points import ExampleDataResponse
 from typing import Callable
 from sklearn.cluster import KMeans
+
+from .routers import base
 
 app = FastAPI(
     title="Test Python Backend",
@@ -17,7 +16,6 @@ app = FastAPI(
                    It provides acess via REST API.""",
     version="0.1.0",
 )
-
 
 # Allow CORS
 app.add_middleware(
@@ -27,43 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.post("/get-data")
-def upload_data(name: str):
-    data = pd.read_csv(f"data/dataset_{name}.csv")
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(data)
-    labels = kmeans.labels_
-    data["cluster"] = labels.tolist()
-    print(data.head())
-    # print(data.to_dict(orient="records"))
-    return data.to_dict(orient="records")
-
-
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    html_content = """
-        <html>
-            <head>
-                <title>Week 2</title>
-            </head>
-            <body>
-                <h1>Test Python Backend</h1>
-                Visit the <a href="/docs">API doc</a> (<a href="/redoc">alternative</a>) for usage information.
-            </body>
-        </html>
-        """
-    return HTMLResponse(content=html_content, status_code=200)
-
+app.include_router(base.router, prefix=os.environ["CONTEXT_PATH"])
 
 def update_schema_name(app: FastAPI, function: Callable, name: str) -> None:
     """
@@ -85,6 +47,5 @@ def update_schema_name(app: FastAPI, function: Callable, name: str) -> None:
             route.body_field.type_.__name__ = name
             break
 
-
-update_schema_name(app, create_file, "CreateFileSchema")
-update_schema_name(app, create_upload_file, "CreateUploadSchema")
+update_schema_name(app, base.create_file, "CreateFileSchema")
+update_schema_name(app, base.create_upload_file, "CreateUploadSchema")

251544a4b65d554a8837938eecd255c7ba80dd20
Alessandro <torci.dev@gmail.com>
2023-01-31 18:04:21 +0100

    Format Code

--- backend-project/setup.py
+++ backend-project/setup.py
@@ -7,18 +7,18 @@ def read(fname):
 
 
 setup(
-    name='backend-project',
-    version='0.1.0',
-    description='backend for the dummy project of the XAI-IML 2023 course.',
-    long_description=read('../README.md'),
+    name="backend-project",
+    version="0.1.0",
+    description="backend for the dummy project of the XAI-IML 2023 course.",
+    long_description=read("../README.md"),
     classifiers=[
-        'Programming Language :: Python :: 3.10',
-        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 4 - Beta",
     ],
     keywords=[],
-    url='',
-    author='',
-    author_email='',
-    packages=find_packages(include=['src']),
-    package_dir={'src': 'src'}
+    url="",
+    author="",
+    author_email="",
+    packages=find_packages(include=["src"]),
+    package_dir={"src": "src"},
 )

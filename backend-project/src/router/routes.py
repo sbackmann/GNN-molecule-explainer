2df6ea16f23493e03b24457f0c26dc8c27f306b2
Alessandro <torci.dev@gmail.com>
2023-02-02 17:35:17 +0100

    Change resource

--- backend-project/src/router/routes.py
+++ backend-project/src/router/routes.py
@@ -1,12 +1,12 @@
 from flask_restful import Api
 import src.resources as res
 
-API = "/api/"
+API = "/api/"  # optional string
 
 
 def add_routes(app):
     api = Api(app)
 
-    api.add_resource(res.ames.AmesData, API + "/<string:source>")
+    api.add_resource(res.ames.local, API + "<access point url>")
 
     return api

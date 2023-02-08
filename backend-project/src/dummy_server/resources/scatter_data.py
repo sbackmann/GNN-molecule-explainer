52257738b3d26ca9e7508ab2b4ad3bb64281a3b4
ChengFR <459763742@qq.com>
2023-02-08 21:50:38 +0100

    add dataset resource

--- backend-project/src/dummy_server/resources/scatter_data.py
+++ backend-project/src/dummy_server/resources/scatter_data.py
@@ -1,38 +1,24 @@
 import os
-from flask_restful import Resource
 import pandas as pd
+
+from flask_restful import Resource
 from sklearn.cluster import KMeans
 
 
 class DatasetResource(Resource):
-    """Abstract template for a dataset resource."""
-    name: str  # abstract attribute
+    """dataset resource."""
     data_root = os.path.join(".", "data")
 
-    def get(self):
-        path_name = os.path.join(self.data_root, f"dataset_{self.name}.csv")
+    def get(self, name):
+        path_name = os.path.join(self.data_root, f"dataset_{name}.csv")
         data = pd.read_csv(path_name)
-        
+
         # process the data, e.g. find the clusters
         kmeans = KMeans(n_clusters=2, n_init=10, random_state=0).fit(data)
-        labels = kmeans.labels_
+        labels = kmeans.labels_.tolist()
         
         # Add cluster to data
-        data["cluster"] = labels.tolist()
+        data["cluster"] = labels
 
         # Convert to dictionary
         return data.to_dict(orient="records")
-
-
-class BlobsResource(DatasetResource):
-    name = "blobs"
-
-
-class CirclesResource(DatasetResource):
-    name = "circles"
-
-
-class MoonsResource(DatasetResource):
-    name = "moons"
-
-
--- backend-project/src/dummy_server/router/routes.py
+++ backend-project/src/dummy_server/router/routes.py
@@ -1,14 +1,12 @@
 from flask_restful import Api
 import dummy_server.resources as res
 
-API = "/api/"  # optional string
+API = "/api/v1/"  # optional string
 
 
 def add_routes(app):
     api = Api(app)
 
-    api.add_resource(res.scatter_data.BlobsResource, API + "blobs")
-    api.add_resource(res.scatter_data.CirclesResource, API + "circles")
-    api.add_resource(res.scatter_data.MoonsResource, API + "moons")
+    api.add_resource(res.scatter_data.DatasetResource, API + "data/<string:name>")
 
     return api

75e4669044801c8538913d1b3c27e29ac9807246
jsanguino <javier.sb98@gmail.com>
2023-02-21 16:29:44 +0100

    Original befre trying to fix networking

--- react-frontend/src/router/apiClient.ts
+++ react-frontend/src/router/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = "http://be.dummy-fullstack.course-xai-iml23.isginf.ch/api/v1"
+export const BASE_URL = "http://localhost:8000/api/v1"
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,

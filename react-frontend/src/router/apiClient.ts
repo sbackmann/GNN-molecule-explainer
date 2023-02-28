af759c16d6545c2b3baa70cb088e4b2c632697c7
jsanguino <javier.sb98@gmail.com>
2023-02-28 14:39:22 +0100

    env variables for local development

--- react-frontend/src/router/apiClient.ts
+++ react-frontend/src/router/apiClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export const BASE_URL = process.env.NODE_ENV==="production"? "http://be.dummy-fullstack.course-xai-iml23.isginf.ch/api/v1":"http://localhost:8000/api/v1"
 
 const axiosClient = axios.create({
@@ -9,3 +10,5 @@ const axiosClient = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+export default axiosClient

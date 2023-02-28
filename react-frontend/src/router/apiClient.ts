1db603d5769c35e8331de661b889c152d309c83a
jsanguino <javier.sb98@gmail.com>
2023-02-28 15:53:01 +0100

    relative url for deployment

--- react-frontend/src/router/apiClient.ts
+++ react-frontend/src/router/apiClient.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 
-const host = window.location.origin;
-console.log(host)
-export const BASE_URL = process.env.NODE_ENV==="production"? "http://be.dummy-fullstack.course-xai-iml23.isginf.ch/api/v1":"http://localhost:8000/api/v1"
+export const BASE_URL = process.env.NODE_ENV==="production"? `http://be.${window.location.hostname}/api/v1`:"http://localhost:8000/api/v1"
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,

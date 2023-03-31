52a05636d785afcd524b100fdd9408fef11148db
sbackmann <sbackmann@student.ethz.ch>
2023-03-31 10:15:26 +0200

    Changed DataPoint interface

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { DataArray } from "./types/data";
+import { DataArray, DataPoint } from "./types/data";
 import DataChoiceComponent from "./components/DataChoice";
-import ScatterPlot from "./components/ScatterPlot";
+// import ScatterPlot from "./components/ScatterPlot";
 import { postPoints } from "./router/resources/data";
 
 function App() {
   const [exampleData, setExampleData] = useState<DataArray>();
   const [dataChoice, setDataChoice] = useState<string>();
+  const [selected, setSelected] = useState<DataPoint>();
 
   useEffect(() => {
     dataChoice &&
       postPoints(dataChoice).then((exampleData) => {
         setExampleData(exampleData);
+        setSelected(exampleData![parseInt(dataChoice, 10)]);
       });
   }, [dataChoice]);
 
@@ -24,9 +26,10 @@ function App() {
     <div className="App">
       <header className="App-header"> Molecule visualisation </header>
       <DataChoiceComponent onChoiceMade={choiceMade} />
-      <ScatterPlot width={1100} height={550} data={exampleData} />
+      
     </div>
   );
 }
-
+// {this.props.selected ? selected!.edge_attr}
+// <ScatterPlot width={1100} height={550} data={exampleData} graphid={dataChoice}/>
 export default App;

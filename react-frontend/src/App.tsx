25b6eabaed6f2bf96fb44f396db559c866ef299b
RushanWang1 <ruswang@student.ethz.ch>
2023-04-16 19:55:20 +0200

    use bootstrap and add first component

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
-import {
-  Badge,
-  Button,
-  Card,
-  Navbar,
-  Nav,
-  Table,
-  Container,
-  Row,
-  Col,
-  Form,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
+// import {
+//   Badge,
+//   Button,
+//   Card,
+//   Navbar,
+//   Nav,
+//   Table,
+//   Container,
+//   Row,
+//   Col,
+//   Form,
+//   OverlayTrigger,
+//   Tooltip,
+// } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { DataArray, DataPoint } from "./types/data";
 import DataChoiceComponent from "./components/DataChoice";
 // import ScatterPlot from "./components/ScatterPlot";
@@ -41,7 +47,8 @@ function App() {
     setDataChoice(choice);
   }
 
-  return (    
+  return (  
+    <>
     <div className="App">
       <header className="App-header"> GNN Explainer </header>      
       
@@ -49,13 +56,30 @@ function App() {
       <p>The selected molecule is toxic:</p>      
       
       {selected ? selected.y : null}
-    </div>
+    </div>    
+    </>  
   );
 }
 
 function Dashboard(){
   return(
-    <Container>
+    <>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Choose a molecule</Card.Title>
+        <Card.Text>
+          Here will be the graph to show whether the current molecule is toxic.
+        </Card.Text>
+        <Button variant="primary">Select</Button>
+      </Card.Body>
+    </Card>
+    {/* <p>Hello World</p> */}
+    {/* <Container>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container> */}
+    {/* <Container fluid>
         <Row>
           <Col>
             <Card>
@@ -66,8 +90,9 @@ function Dashboard(){
             </Card>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
+      </>
   )
 }
 // <ScatterPlot width={1100} height={550} data={exampleData} graphid={dataChoice}/>
-export default App;
+export default Dashboard;

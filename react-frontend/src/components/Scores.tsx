88ec253c05edcc370fffef24b26aa0d5b6bcd67c
kamara1 <kenza.amara@ai.ethz.ch>
2023-05-30 17:37:54 +0200

    new description

--- react-frontend/src/components/Scores.tsx
+++ react-frontend/src/components/Scores.tsx
@@ -65,21 +65,28 @@ const ComputeScores: React.FC<ScoresProps> = ({
         <ListGroupItem>
           <div style={{ display: "flex", alignItems: "center" }}>
             <span style={{ marginRight: "10px", minWidth: "80px" }}>
-              Characterization: {scores["charact_prob"]}
+              Characterization: {scores["charact_acc"]}
             </span>
           </div>
         </ListGroupItem>
         <ListGroupItem>
           <div style={{ display: "flex", alignItems: "center" }}>
             <span style={{ marginRight: "10px", minWidth: "80px" }}>
-              Necessary explanation: {scores["fidelity_prob+"]}
+              <Form.Label
+                style={{ marginRight: "10px", minWidth: "150px" }}
+                data-bs-toggle="tooltip"
+                data-bs-placement="top"
+                title="An explanation is necessary if the model prediction changes when you remove it from the initial graph. Necessary explanations are similar to counterfactual explanations. A necessary explanation has a Fidelity+ score close to 1."
+              >
+                Necessary explanation: {scores["fidelity_acc+"]}
+              </Form.Label>
             </span>
           </div>
         </ListGroupItem>
         <ListGroupItem>
           <div style={{ display: "flex", alignItems: "center" }}>
             <span style={{ marginRight: "10px", minWidth: "80px" }}>
-              Sufficient explanation: {1 - scores["fidelity_prob-"]}
+              Sufficient explanation: {1 - scores["fidelity_acc-"]}
             </span>
           </div>
         </ListGroupItem>

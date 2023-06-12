f01650e45b6e1edf0a26168e32b85a595f0cb6e9
sbackmann <sbackmann@student.ethz.ch>
2023-06-12 11:34:56 +0200

    Removed scatterplot ticks

--- react-frontend/src/components/ScatterPlot.tsx
+++ react-frontend/src/components/ScatterPlot.tsx
@@ -146,13 +146,13 @@ function renderScatterPlot(
 
   // Add inline styles to hide axis lines
   getChildOrAppend<SVGGElement, SVGGElement>(base, "g", "y-axis-base")
-    .call(d3.axisLeft(y).ticks(4).tickFormat(() => ""))
+    .call(d3.axisLeft(y).ticks(4).tickSize(0).tickFormat(() => ""))
     .selectAll("path")
     .style("display", "none");
 
   getChildOrAppend<SVGGElement, SVGGElement>(base, "g", "x-axis-base")
     .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).ticks(5).tickFormat(() => ""))
+    .call(d3.axisBottom(x).ticks(5).tickSize(0).tickFormat(() => ""))
     .selectAll("path")
     .style("display", "none");
 

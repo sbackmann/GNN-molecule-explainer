3cf06e09a7a8075358d73a87420a810195629ae1
sbackmann <sbackmann@student.ethz.ch>
2023-06-14 15:57:22 +0200

    Changed requirements installation for deployment

--- backend-project/setup.py
+++ backend-project/setup.py
@@ -43,6 +43,9 @@ setup(
         "flask-cors>=3.0.10,<3.1",
         "pandas>=1.4.1,<1.5",
         "scikit-learn>=1.0.2,<1.1",
+        "torch==1.11.0",
+        "torch-geometric==2.0.4",
+
     ],
     packages=find_packages(where="src", include=["dummy_server*"]),
     package_dir={"": "src"},
